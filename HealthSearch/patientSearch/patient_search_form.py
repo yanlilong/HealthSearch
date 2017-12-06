e8c02001f99b7d38a7e0b4dcd44bb570ba37e223
yanlilong <yan.w.li@outlook.com>
2017-12-06 08:50:15 +0100

    update search_form

--- HealthSearch/patientSearch/patient_search_form.py
+++ HealthSearch/patientSearch/patient_search_form.py
@@ -1,27 +1,27 @@
 from django.forms import ModelForm, Textarea, Select
 from django import forms
 from models import Query, DataResource
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 
 class QueryFrom(ModelForm):
   class Meta:
     model = Query
-    fields = ('age', 'sex', 'sympo', 'disease',)
-    SEX_CHOICES = ((True, 'Female'),
-                   (False, 'Male'))
+    labels = {
+      'age': "Age",
+      'sex': "Gender",
+      'sympo': 'Sympotom',
+      'disease': 'Disease'
+    }
+    fields = ('age', 'sex', 'sympo', 'disease')
+
+    SEX_CHOICES = (
+      ('True', 'Female'),
+      ('False', 'Male'),
+    )
+
     widgets = {
-      'sex': Select(attrs={'class': 'select',
-                           'choices': SEX_CHOICES,
-                           'initial': 'Female',
-                           'cols':3,
-                           'rows':3}),
+      'sex': Select(attrs={'class': 'select'}, choices=SEX_CHOICES),
       'sympo': Textarea(attrs={'cols': 30, 'rows': 2}),
       'disease': Textarea(attrs={'cols': 30, 'rows': 2}),
-    }
-    labels = {
-      'age': _('Age'),
-      'sex': _('Gender'),
-      'sympo': _('Sympotom'),
-      'disease': _('Disease'),
     }
