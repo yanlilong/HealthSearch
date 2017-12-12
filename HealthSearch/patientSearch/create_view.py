849c36ce0b745f9b7b5e8d412af1a6195c75b39c
yanlilong <yan.w.li@outlook.com>
2017-12-12 14:09:43 +0100

    save new Query object,get url of DataResource with selected DataResource name and change sex with Female or Male,bu not True or False

--- HealthSearch/patientSearch/create_view.py
+++ HealthSearch/patientSearch/create_view.py
@@ -1,9 +1,44 @@
-from models import DataResource
+from models import Query, DataResource
+from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
+import pdb
+import logging
+logger = logging.getLogger('patientSearch')
+
+
+def save_query(age, sex, sympo, disease):
+  try:
+    query = Query.objects.get(age=age, sex=sex, sympo=sympo, disease=disease)
+    query.freq = query.freq + 1
+    query.query_last_time = timezone.now()
+  except ObjectDoesNotExist:
+    logger.info(msg="a new Queryparameter will be saved in database")
+    query = Query(age=age, sex=sex, sympo=sympo, disease=disease,
+                  query_last_time=timezone.now(), freq=0)
+
+  query.save()
+
+
+def get_dataresource(name):
+  try:
+    dataresource = DataResource.objects.get(resource_name=name)
+    logger.info(msg=dataresource.url)
+    return dataresource.url
+  except ObjectDoesNotExist:
+    logger.error(msg="selected Dataresource is not exist")
+    # return a default url
+    return None
 
 
 def create_resource():
   resource_name_list = []
   for data_resource in DataResource.objects.all():
     resource_name_list.append(data_resource.resource_name)
-    print(resource_name_list)
   return resource_name_list
+
+
+def create_sex(sex):
+  if (sex == True):
+    return "Female"
+  elif (sex == False):
+    return "Male"
